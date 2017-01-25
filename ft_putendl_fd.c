2cb2f06faf0eddc70f1742009b051f5bffc12da6
Syberam <sbonnefo@student.42.fr>
2017-01-25 17:34:47 +0100

    simplification using the new ft_putstr

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: sbonnefo <sbonnefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/08 19:19:56 by sbonnefo          #+#    #+#             */
-/*   Updated: 2016/12/08 19:22:14 by sbonnefo         ###   ########.fr       */
+/*   Updated: 2017/01/25 17:34:15 by sbonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,6 @@
 
 void	ft_putendl_fd(char const *s, int fd)
 {
-	int		i;
-
-	i = 0;
-	while (s[i] != 0)
-	{
-		ft_putchar_fd(s[i], fd);
-		i++;
-	}
+	ft_putstr_fd(s, fd);
 	ft_putchar_fd('\n', fd);
 }
